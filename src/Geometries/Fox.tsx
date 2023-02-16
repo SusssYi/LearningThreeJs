2ab1dcbd22000c46fba7b43297e1eceaa6be8b8d
Sus <qq945792171@gmail.com>
2023-02-16 20:08:41 +0800

    FoxAnimationsFinal

--- src/Geometries/Fox.tsx
+++ src/Geometries/Fox.tsx
@@ -1,11 +1,30 @@
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
 import type { MeshProps } from "@react-three/fiber";
-
+import { useControls } from "leva";
+import { useEffect } from "react";
 const Fox = (props: MeshProps) => {
-  const { scene } = useGLTF("/Fox/glTF/Fox.gltf");
+  const fox = useGLTF("/Fox/glTF/Fox.gltf");
+  const animations = useAnimations(fox.animations, fox.scene);
+  const { animationName } = useControls("animations", {
+    animationName: { options: ["Run", "Walk", "Survey"] },
+  });
+
+  useEffect(() => {
+    const action = animations.actions[animationName];
+    if (action) {
+      //  add new animations fadeIn
+      action.fadeIn(0.5).play();
+    }
+
+    return () => {
+      // let old animations fadeOut
+      action?.fadeOut(0.5);
+    };
+  }, [animationName]);
+
   return (
     <>
-      <primitive {...props} object={scene} />
+      <primitive {...props} object={fox.scene} />
     </>
   );
 };
